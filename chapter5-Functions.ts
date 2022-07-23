d2ee9df91c8b77d6f0341f2ec10f0fa9a40831ee
Juan Canelon <liscanojac@gmail.com>
2022-07-23 16:27:55 -0300

    feat: chapter 5

---
+++ chapter5-Functions.ts
@@ -0,0 +1,4 @@
+
+
+// this is to avoid collision of variable names in other files, the export force it to be a module and now you dont have to worry about tyhe same variable names in other files
+export {};
