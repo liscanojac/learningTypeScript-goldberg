5227bebc47e0d41f30cd2b7d65e68ce6326d9c78
Juan Canelon <liscanojac@gmail.com>
2022-07-23 16:33:18 -0300

    refactor: adding export

--- chapter5-Functions.ts
+++ chapter5-Functions.ts
@@ -1,4 +1,4 @@
-
+// sidfud
 
 // this is to avoid collision of variable names in other files, the export force it to be a module and now you dont have to worry about tyhe same variable names in other files
 export {};
